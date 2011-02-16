5d48ad4f65fa7c3f5704f048ea51228096f2961d
Sam Livingston-Gray <sam@renewfund.com>
2011-02-16 13:52:02 -0800

    tweak whitespace for enhanced "show invisibles" demo

--- examples/whitespace.rb
+++ examples/whitespace.rb
@@ -14,14 +14,15 @@ namespace :deploy do
     transaction do
       puts "Deploying #{release_number} to #{stage}."
       time.begin
-		  make_shared_dirs
+	    make_shared_dirs
 		  misc.rollback_notification # this task only exists to send notifications in case the deploy fails
-		  web.disable
-		  background_updater.disable
-      stop
+			web.disable
+  	  background_updater.disable
+  	  stop
       update
       write_version_file
-      top.deploy.db.reset_db_hook
+      top.deploy.db.reset_db_hook     
+                              #  ^^^^^
       # top.deploy.tables # uncomment to pull in property data updates when Darrell says you should
       top.deploy.migrate
       top.deploy.db.functions
@@ -55,28 +56,10 @@ namespace :deploy do
     ENV['SKIP_BOOTSTRAP'] = 'true'
     deploy.default
   end
-
+  
   desc "Installs CMS configuration in target environment.  Override me in config/deploy/ENV.rb for special configurations!"
   task :setup_cms_config do
     run "cp #{release_path}/config/cms_config.deploy.yml #{release_path}/config/cms_config.yml"
   end
 
-  desc "Issues a subscription request to the CMS"
-  task :subscribe_to_cms do
-    run "cd #{current_path} && env RAILS_ENV=#{rails_env} script/runner CmsGateway::Remote.subscribe_to_cms"
-  end
-
-  desc "create htaccess file in public folder"
-  task :create_htaccess do
-    run "cd #{release_path};RAILS_ENV=#{rails_env} script/build_htaccess" 
-    unless remote_file_exists?("#{release_path}/public/.htaccess")
-      raise 'Error: .htaccess not generated'
-    end
-  end
-  
-  desc "clear fragment cache" 
-  task :clear_fragment_cache do
-    run "cd #{current_path} && env RAILS_ENV=#{rails_env} rake tmp:frag:clear"
-  end
-
-end # namespace :deploy
+end
